040d0345f1393e70dbd1311c834bcd17318ccb29
Kim Wysocki <kcwysocka@icloud.com>
2020-06-12 15:41:08 -0400

    add further enzyme tests for swarmDeployment component

--- __tests__/swarmDeploymentTests.test.tsx
+++ __tests__/swarmDeploymentTests.test.tsx
@@ -2,20 +2,26 @@ import DeploySwarm from '../src/renderer/components/SwarmDeployment';
 import { shallow, mount } from 'enzyme';
 
 describe('Swarm Deployment component', () => {
-  it('should have a button with className `deploy-btn`', () => {
-    expect(1).toEqual(1);
-    
-  });
-  // const props = {
-  //   currentFile: 'User/project_name/docker-compose.yml'
-  // }
-  // const mockFunction = jest.fn(() => {});
-  // const wrapper = shallow(<DeploySwarm {...props} />);
+  const props = {
+    currentFile: 'User/project_name/docker-compose.yml'
+  }
+  const mockFunction = jest.fn(() => {});
+  const wrapper = shallow(<DeploySwarm {...props} />);
   
-  // it('should have a button with className `deploy-btn`', () => {
-  //   let calls = mockFunction.mock.calls.length;
-  //   expect(wrapper.find('deploy-btn')).toHaveLength(1);
-  //   wrapper.find('deploy-btn').simulate('click');
-  //   expect(mockFunction.mock.calls.length).toEqual(calls + 1);
-  // });
+  describe('Component content', () => {
+    it('should have a button with className `deploy-btn`', () => {
+      let calls = mockFunction.mock.calls.length;
+      expect(wrapper.find('deploy-btn')).toHaveLength(1);
+      wrapper.find('deploy-btn').simulate('click');
+      expect(mockFunction.mock.calls.length).toEqual(calls + 1);
+    });
+  
+    it('should have a pop-up box that contains further actions/information', () => {
+      expect(wrapper.find('swarm-deploy-popup')).toHaveLength(1);
+    });
+  
+    it('allows user to enter a name for their stack(s)', () => {
+      expect(wrapper.find('input')).toHaveLength(1);
+    });
+  });
 });
